a60ab940f203bb66b01495e7fe3032fea4e9222e
arnaubrosa <79318501+arnaubrosa@users.noreply.github.com>
2021-03-16 10:23:45 +0100

    Update Line.java

--- Line.java
+++ Line.java
@@ -1,36 +1,44 @@
 import java.io.*;
+import java.util.*;
 
-class Line {
-	public void fletxaDalt(){
-		
-	}
-
-	public void fletxaBaix(){
+class Line extends Observable {
 	
-	}
+	private int pos=0;
+	public StringBuffer llargada= new StringBuffer(1000);
+
 	
 	public void fletxaDreta(){
-	
+		if(pos!=llargada.length()){
+		this.pos++;
+		System.out.print("\033[C");
+		}
 	}
 	
 	public void fletxaEsquerra(){
-	
+		if(pos!=0){
+		this.pos--;
+		System.out.print("\033[D");
+		}
 	}
 	
 	public void home(){
-	
+		for(int i=0;i<this.pos;i++){
+		System.out.print("\033[D");
+		} this.pos=0;
 	}
 	
 	public void end(){
-	
+		for(int i=0;i<llargada.length()-this.pos;i++){
+		System.out.print("\033[C")
+		} this.pos=this.llargada.length();
 	}
 	
 	public void insert(){
-	
+		System.out.print("\033[2");
 	}
 	
 	public void del(){
-	
+		System.out.print("\033[3");
 	}
 	
 	public void bksp(){
@@ -38,5 +46,7 @@ class Line {
 		System.out.print(" ");    //posar espai
 		System.out.print("\b");   //tirar enrere un altre cop
 	}
+	public int getLength{
 
-}
+	return llargada.length();
+	}

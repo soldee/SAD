91369fc35d7b36b04ddd16fe2cc99a7b9ed995d2
soldee <71665509+soldee@users.noreply.github.com>
2021-06-11 14:01:59 +0200

    Add files via upload

---
+++ client/MySocket.java
@@ -0,0 +1,32 @@
+import java.net.*;
+import java.io.*;
+
+public class MySocket extends Socket {
+    private InputStream inputstream;
+    private OutputStream outputstream;
+    private BufferedReader read;
+    private PrintWriter write;
+    public String nom;
+
+    public MySocket(String host, int port, String nom) throws IOException, UnknownHostException, ConnectException {
+        super(host,port);
+        this.nom = nom;
+
+        inputstream = this.getInputStream();
+        outputstream = this.getOutputStream();
+        
+        read = new BufferedReader(new InputStreamReader(inputstream));
+        write = new PrintWriter(outputstream,true);
+        
+        escriure(nom);       
+    }
+
+    public String llegir() throws IOException{ //llegir el que arriba del servidor
+        String miss = read.readLine();
+        return miss;
+    }
+
+    public void escriure(String outputstream) throws IOException{ //escriu al servidor
+        write.println(outputstream);
+    }
+}
